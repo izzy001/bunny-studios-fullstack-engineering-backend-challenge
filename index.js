7bdc06c72c3403c5167558840f50491f30e1ee77
israel emoitologa <israelemoitologa@israels-MacBook-Pro-2.local>
2021-11-02 12:37:43 +0100

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}))
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
